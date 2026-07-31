67cecf78d79dbf6b1877f531c24e9484161840f1
Suraj Verma <suraj.verma1@ext.boots.com>
2026-07-31 11:11:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aab8fa8b80a1580ded3bb76d6fcdd090547a9ee54ff98389bf3a4935c4074fe1
+size 6634
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef8363931eadfa3456ad1d50ad851d83f48791988c5babf3cd384165e6e031c6
+size 2048
